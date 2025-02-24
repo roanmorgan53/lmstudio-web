ef069d76834d9e717013ca1bf5a4d57c3f0f09c4
roanmorgan53 <roanlm007@gmail.com>
2025-02-24 16:45:21 -0500

    little QOL changes

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,14 @@
-async function GetData() {
-  const message = ["Introduce yourself"];
-  let endpoint = "http://127.0.0.1:1234/v0/chat/completions";
-  const request = await fetch(endpoint, {method: 'POST'});
-  const response = await request.json();
-  console.log(response);
-}
+const submitButton = document.getElementById("submitButton");
+const inputfield = document.getElementById("userInput");
+
 
 async function postData(){
   try {
     let endpoint = "http://127.0.0.1:1234/v1/chat/completions";
 
-    const postData  = {
+    const data  = {
       "model": "gemma-2-2b-it",
-      "messages": [
-        {"role": "user", "content": "Introduce yourself."}
-      ],
+      "messages": messageList,
       "temperature": 0.7,
       "max_tokens": -1,
       "stream": false
@@ -26,7 +20,7 @@ async function postData(){
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(postData)
+      body: JSON.stringify(data)
     });
 
     const response = await request.json();
@@ -38,10 +32,23 @@ async function postData(){
 }
 
 async function handleResponse() {
-  const reply = await postData();
+  const reply = await postData(messageList);
   const repliedMessage = reply.choices[0].message.content;
   console.log(repliedMessage);
   document.getElementById("output").textContent = repliedMessage;
+  messageList.push({ "role": "assistant", "content": repliedMessage });
 }
 
-handleResponse();
+const messageList = [
+  { "role": "system", "content": "You are a wizard from mordor"},
+];
+
+function processNewMessage(){
+  console.log("Processing message");
+  const input = inputfield.value;
+  inputfield.value = "";
+  
+  messageList.push({"role": "user", "content": input});
+  handleResponse(messageList);
+}
+
